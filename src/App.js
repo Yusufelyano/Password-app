a5320edd2290f9e0d548509e761a28e25f38fd44
Yusuf <yusufelyano@gmail.com>
2021-05-25 17:57:31 +0300

    Password-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [value, setValue] = useState("")
+  const handlePassword = (e) => {
+    setValue(e.target.value)
+  }
+  const [valueTwo, setValueTwo] = useState("")
+  const handlePasswordTwo = (e) => {
+    setValueTwo(e.target.value)
+  }
+  const [result, setResult] = useState("")
+  const handleResult = () =>{
+    if(value !== valueTwo) {
+      return setResult("Пароли не совпадают")
+    }
+    return setResult(value) 
+  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app" >
+      <input 
+      type="text"
+      value={value}
+      onChange={handlePassword}
+      placeholder="Введите пароль"
+      />
+      <input 
+      type="text"
+      value={valueTwo}
+      onChange={handlePasswordTwo}
+      placeholder="Повторите пароль"
+      />
+      <button onClick={handleResult}>Результат</button>
+      <div>{result}</div>
+      
     </div>
   );
 }
